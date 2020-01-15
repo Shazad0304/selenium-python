40d661d20679a42129943383a2ef67b32ba13e5b
Shazad0304 <50071791+Shazad0304@users.noreply.github.com>
2020-01-15 16:49:23 -0500

    Update sel-dice.py

--- sel-dice.py
+++ sel-dice.py
@@ -13,19 +13,20 @@ noResume = input('please enter the number of consultants: ')
 minExp = input('enter min experience: ')
 maxExp = input('enter max experience: ')
 visas = input('Please enter visa status (for two i.e need h1|employment auth document): ').split('|')
+pageNo=input('please enter page number: ')
 firstVisa = visas[0]
 secVisa = ''
 if len(visas) > 1:
     secVisa = '%7C'+visas[1]
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
-driver = webdriver.Chrome(executable_path='./chromedriver1',options=chrome_options)
-driver.get('https://www.dice.com/employer/talent/search;q='+skill+';page=1;pageSize='+noResume+';location='+location+';sortBy=relevance;sortByDirection=desc;workPermits='+firstVisa+secVisa+';yOEMin='+minExp+';yOEMax='+maxExp+';excludeRecruiters=true;contactInfo=email;lastActive=90;profileSource=resumes')
-WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.ID,'username'))) 
+driver = webdriver.Chrome(executable_path='/home/shazad/Desktop/py/chromedriver1',options=chrome_options)
+driver.get('https://www.dice.com/employer/talent/search;q='+skill+';page='+pageNo+';pageSize='+noResume+';location='+location+';sortBy=relevance;sortByDirection=desc;workPermits='+firstVisa+secVisa+';yOEMin='+minExp+';yOEMax='+maxExp+';excludeRecruiters=true;contactInfo=email;lastActive=90;profileSource=resumes')
+WebDriverWait(driver,200).until(EC.presence_of_all_elements_located((By.ID,'username'))) 
 driver.find_element_by_id('username').send_keys('imran@prosistech.com')
 driver.find_element_by_id('password').send_keys('Prosis2019')
 driver.find_element_by_xpath("//button[@type='submit']").click() 
-WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'search-results'))) 
+WebDriverWait(driver,200).until(EC.presence_of_all_elements_located((By.CLASS_NAME,'search-results'))) 
 a = driver.find_element_by_class_name('search-results').get_attribute('innerHTML')
 soup = BeautifulSoup(a,features="html.parser")
 for o in soup.find_all('a', attrs={'class': 'view-link ng-star-inserted'},href=True):
@@ -37,7 +38,7 @@ worksheet = workbook.add_worksheet()
 row = 0
 for items in urls:
     driver.get(items)
-    WebDriverWait(driver,20).until(EC.presence_of_all_elements_located((By.ID,'profile-page-info-name'))) 
+    WebDriverWait(driver,200).until(EC.presence_of_all_elements_located((By.ID,'profile-page-info-name'))) 
     name = driver.find_element_by_id('profile-page-info-name').text
     details = driver.find_elements_by_xpath("//a[@class='c-pointer text-primary']")
     print(name + ' '+ details[0].text)
@@ -52,3 +53,4 @@ for items in urls:
 
 workbook.close()
 print('DONE' + ' '+ fileName)
+    
